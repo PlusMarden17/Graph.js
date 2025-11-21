7f2720c46c088d2454a942a4ed7d4fdae1845f17
Coconut153 <128516364+PlusMarden17@users.noreply.github.com>
2025-11-21 13:38:11 +0200

    Init commit

---
+++ src/Docs.md
@@ -0,0 +1,65 @@
+# Graph.js documentation
+
+---
+
+The content of documentation:
+
+* [What is Graph.js](#what-is-graphjs)
+* [Installation](#install-library)
+* [Getting started](#getting-started)
+    * [Graph arguments](#graph-arguments)
+    * [Drawing lines](#drawing-lines)
+
+---
+
+## What is Graph.js
+
+Graph.js - is the JavaScript graph library
+
+[Back to content ->](#graph.js-documentation)
+
+---
+
+## Installation
+
+To install Graph.js you need:
+1. **Download** `graph.js` file on github
+2. **Place** it in your project (Better create /libs folder in your project and put `graph.js` file into there)
+3. **Add** script usage to your HTML file (If you placed the library file into the /libs folder, add this line to the `<head>` section of your HTML: `<script src="/libs/graph.js"></script>`)
+
+**Done!** Now you can use library...
+
+[Back to content ->](#graph.js-documentation)
+
+---
+
+## Getting started
+
+To start working with Graph.js you need to create graphs container in your HTML file, example:
+`<div id="graphs"></div>`
+You can choose ID what ever you want.
+Now let's create a new graph and add it to graphs container, example:
+> const g1 = newGraph("test", 100, 200);
+> const ctx1 = g1.ctx;
+> addGraph("test", "graphs");
+In first line we created new graph with name test and ID g1, then we created ctx and assigned that to our graph. And in last line we added graph to graphs container.
+
+### Graph arguments
+
+When we created a new graph we entered 3 arguments "test", 100 and 200, this is what it means:
+`"test"` - Graphs name
+`100` - Graphs height
+`200` - Graphs width
+Then in third line we added graph to containe, this is what the arguments means:
+`"test"` - Graphs name
+`"graphs"` - Container name
+You can enter different values of arguments but this is an example.
+
+### Drawing lines
+
+Now you can draw lines in your graph. Do to that you need to create a new node and then draw it, example:
+> const n1 = newNode("testNode", 50, 100);
+> drawNode("testNode", "test");
+Where "testNode" is the node name, 50 - y value and 100 - x value
+
+---
---
+++ src/graph.js
@@ -0,0 +1,85 @@
+const graphs = {};
+const nodes = {};
+
+class GraphInstance {
+    constructor(canvas, ctx) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+    }
+}
+
+class NodeInstance {
+    constructor(nodeName, y , x) {
+        this.nodeName = nodeName;
+        this.y = y;
+        this.x = x;
+    }
+}
+
+function newGraph(name, height, width) {
+    const canvas = document.createElement("canvas");
+    const id = `graph-${name.toLowerCase().replace(/\s+/g, '-')}`;
+
+    canvas.id = id; 
+    canvas.height = height;
+    canvas.width = width;
+    canvas.classList.add("graph-canvas");
+
+    const ctx = canvas.getContext('2d');
+    const gInst = new GraphInstance(canvas, ctx);
+
+    graphs[name] = gInst;
+
+    return gInst;
+}
+
+function addGraph(name, targetElemID) {
+    const gInst = graphs[name];
+    const targetElem = document.getElementById(targetElemID);
+    const canvas = gInst.canvas;
+
+    if (!gInst) {
+        console.error("Graph.js Error: Graph with name " + name + " not found!");
+        return null;
+    }
+
+    if (!canvas || !(canvas instanceof Node)) {
+        console.error("Graph.js Internal Error: Retrieved graph instance does not contain a valid Canvas Node!");
+        return null;
+    }
+
+    if (!targetElem) {
+        console.error("Graph.js Error: Element with ID " + targetElemID + " not found!");
+        return null;
+    }
+
+    if (targetElem.contains(canvas)) {
+        console.warn("Graph.js Warning: Graph with name " + name + " already exists in container " + targetElemID);
+        return canvas;
+    }
+
+    targetElem.appendChild(canvas);
+    
+    return canvas;
+}
+
+function newNode(nodeName, y, x) {
+    const nInst = new NodeInstance(nodeName, y, x);
+    nodes[nodeName] = nInst;
+    return nInst;
+}
+
+function drawNode(nodeName, name) {
+    const gInst = graphs[name];
+    const nInst = nodes[nodeName];
+    const ctx = gInst.ctx;
+
+    if (!nInst) {
+        console.error("Graph.js Error: Node with name " + nodeName + " not found!");
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(nInst.y, nInst.x);
+    ctx.stroke();
+}
